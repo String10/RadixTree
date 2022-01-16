27c1b08733224b2ec847b2beba2c83be50783da2
String Max <mr.tens@foxmail.com>
2022-01-16 19:13:11 +0800

    Add 2 TODO.

--- rdx_node.cpp
+++ rdx_node.cpp
@@ -5,6 +5,8 @@ namespace RadixTree {
     struct rdx_node {
         std::string label;
         bool is_end_of_node;
+        /* TODO: Use rdx_node* instead of rdx_node to avoid recusive copy. */
+        /* TODO: Use smarter data structrue to find next node faster. */
         std::vector <rdx_node> sub_nodes;
         rdx_node(): label(""), is_end_of_node(false) {
             sub_nodes.clear();
