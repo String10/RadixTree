071fa6cc6bf591b5df4321b9929f26fc7bee37b3
String Max <mr.tens@foxmail.com>
2022-01-17 18:01:28 +0800

    Add benchmark, maybe.

--- test.cpp
+++ test.cpp
@@ -1,8 +1,9 @@
 #include "rdx_tree.cpp"
 
-#define TEST_FINDSUCCESSOR
+#define TEST_BENCHMARK
 
 #include <iostream>
+#include <fstream>
 #include <set>
 #include <cstdlib>
 #include <ctime>
@@ -86,4 +87,104 @@ int main() {
         cout << rdx_tree.findSuccessor(word) << endl;
     }
 #endif // TEST_FINDSUCCESSOR
+#ifdef TEST_BENCHMARK
+    srand(time(NULL));
+
+    RadixTree::rdx_tree rdx_tree;
+    set <string> check_helper;
+
+    ofstream of_data("data.txt");
+    
+    constexpr size_t inserts = 1e4, lookups = 1e4, removes = 5e3;
+    string word;
+    for(int i = 0; i < inserts; i++) {
+        word = getRandomString();
+        // rdx_tree.insert(word), check_helper.insert(word);
+        check_helper.insert(word);
+        of_data << word << endl;
+    }
+    for(int i = 0; i < lookups; i++) {
+        word = getRandomString();
+        if(0 == rand() % 2) {
+            for(auto &str: check_helper) {
+                if(0 == rand() % 3) {
+                    word = str;
+                }
+            }
+        }
+        // if(rdx_tree.lookup(word) != check_helper.count(word)) {
+        //     cout << "Insert Test Failed!" << endl;
+        // }
+        of_data << word << endl;
+    }
+
+    for(int i = 0; i < removes; i++) {
+        word = getRandomString();
+        if(0 != rand() % 4) {
+            for(auto &str: check_helper) {
+                if(0 == rand() % 4) {
+                    word = str;
+                    break;
+                }
+            }
+        }
+        // rdx_tree.remove(word), check_helper.erase(word);
+        // if(rdx_tree.lookup(word) != check_helper.count(word)) {
+        //     cout << "Remove Test Failed!" << endl;
+        // }
+        check_helper.erase(word);
+        of_data << word << endl;
+    }
+
+    of_data.close();
+
+    ifstream if_data("data.txt");
+
+    check_helper.clear();
+
+    time_t rdx_tree_start = clock();
+
+    for(int i = 0; i < inserts; i++) {
+        if_data >> word;
+        rdx_tree.insert(word);
+    }
+    for(int i = 0; i < lookups; i++) {
+        if_data >> word;
+        rdx_tree.lookup(word);
+    }
+
+    for(int i = 0; i < removes; i++) {
+        if_data >> word;
+        rdx_tree.remove(word);
+    }
+
+    time_t rdx_tree_end = clock();
+
+    if_data.open("data.txt");
+    time_t check_helper_start = clock();
+
+    for(int i = 0; i < inserts; i++) {
+        if_data >> word;
+        check_helper.insert(word);
+    }
+    for(int i = 0; i < lookups; i++) {
+        if_data >> word;
+        check_helper.count(word);
+    }
+
+    for(int i = 0; i < removes; i++) {
+        if_data >> word;
+        check_helper.erase(word);
+    }
+    
+    time_t check_helper_end = clock();
+
+    if_data.close();
+
+    ofstream logger("log_0.txt");
+
+    logger << "Test Finished!" << endl;
+    logger << "rdx_tree costs: " << rdx_tree_end - rdx_tree_start << "ms" << endl;
+    logger << "check_helper costs: " << check_helper_end - check_helper_start << "ms" << endl;
+#endif // TEST_BENCHMARK
 }
